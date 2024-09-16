0be43ff572326af29bf871463d095f90a96690c3
Guy-BP <122686719+Guy-BP@users.noreply.github.com>
2024-09-16 15:49:59 +0300

    Update app.py

--- LeumiLod/app.py
+++ LeumiLod/app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def hello():
+    return "Hello, HTTPS Flask!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000, ssl_context=('/etc/ssl/certs/flask.crt', '/etc/ssl/private/flask.key'))
